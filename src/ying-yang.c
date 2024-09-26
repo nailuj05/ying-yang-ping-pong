393e4df433e03144ded983681ad1938984cb1e5b
julian <julian.limburg@gmx.de>
2024-09-26 11:27:37 +0200

    more balls and grid collisions

--- src/ying-yang.c
+++ src/ying-yang.c
@@ -6,7 +6,20 @@
 #define SW 900
 #define GRIDNUM 9
 #define CELL SW / GRIDNUM
-#define GRAVITY 9.8f
+#define GRAVITY 0.0f
+#define BALLS 2
+
+typedef struct ball {
+  Vector2 position;
+  Vector2 velocity;
+  bool color;
+} Ball;
+
+float fsign(float x) {
+  if (x > 0.0f)
+    return 1;
+  return -1;
+}
 
 int main(int argc, char *argv[]) {
   InitWindow(SW, SW, "ying yang ping pong");
@@ -22,8 +35,9 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  Vector2 ball = {10.0f, 10.0f};
-  Vector2 vel = {10.0f, 10.0f};
+  Ball *balls = (Ball *)malloc(BALLS * sizeof(Ball));
+  balls[0] = (Ball){10.0f, 10.0f, 10.0f, 10.0f, true};
+  balls[1] = (Ball){SW - 10.0f, SW - 10.0f, -10.0f, -10.0f, false};
 
   Vector2 grav = {0.0f, 0.0f};
 
@@ -32,6 +46,19 @@ int main(int argc, char *argv[]) {
 
     for (size_t x = 0; x < GRIDNUM; ++x) {
       for (size_t y = 0; y < GRIDNUM; ++y) {
+        // Grid Collisions
+        Rectangle rec = {x * CELL, y * CELL, CELL, CELL};
+        for (size_t i = 0; i < BALLS; ++i) {
+          if (grid[x][y] != balls[i].color && CheckCollisionPointRec(balls[i].position, rec)) {
+            if (fabs(balls[i].velocity.x) > fabs(balls[i].velocity.y))
+              balls[i].velocity = Vector2Reflect(balls[i].velocity, (Vector2){1.0f * fsign(balls[i].velocity.x), 0.0f});
+            else
+              balls[i].velocity = Vector2Reflect(balls[i].velocity, (Vector2){0.0f, 1.0f * fsign(balls[i].velocity.x)});
+            grid[x][y] = balls[i].color;
+          }
+        }
+
+        // Grid Rendering
         if (grid[x][y])
           DrawRectangle(x * CELL, y * CELL, CELL, CELL, WHITE);
         else
@@ -39,21 +66,27 @@ int main(int argc, char *argv[]) {
       }
     }
 
+    // Side Collisions and Rendering
     grav.y = GRAVITY * GetFrameTime();
 
-    vel = Vector2Add(vel, grav);
-    ball = Vector2Add(ball, vel);
+    for (size_t i = 0; i < BALLS; ++i) {
+      balls[i].velocity = Vector2Add(balls[i].velocity, grav);
+      balls[i].position = Vector2Add(balls[i].position, balls[i].velocity);
 
-    if (ball.y >= SW)
-      vel = Vector2Reflect(vel, (Vector2){0, 1});
-    if (ball.y <= 0)
-      vel = Vector2Reflect(vel, (Vector2){0, -1});
-    if (ball.x >= SW)
-      vel = Vector2Reflect(vel, (Vector2){1, 0});
-    if (ball.x <= 0)
-      vel = Vector2Reflect(vel, (Vector2){-1, 0});
+      if (balls[i].position.y >= SW)
+        balls[i].velocity = Vector2Reflect(balls[i].velocity, (Vector2){0, 1});
+      if (balls[i].position.y <= 0)
+        balls[i].velocity = Vector2Reflect(balls[i].velocity, (Vector2){0, -1});
+      if (balls[i].position.x >= SW)
+        balls[i].velocity = Vector2Reflect(balls[i].velocity, (Vector2){1, 0});
+      if (balls[i].position.x <= 0)
+        balls[i].velocity = Vector2Reflect(balls[i].velocity, (Vector2){-1, 0});
 
-    DrawCircleV(ball, 10, RED);
+      if (balls[i].color)
+        DrawCircleV(balls[i].position, 10, BLACK);
+      else
+        DrawCircleV(balls[i].position, 10, WHITE);
+    }
 
     DrawFPS(10, 10);
 
@@ -61,6 +94,9 @@ int main(int argc, char *argv[]) {
   }
 
   CloseWindow();
+
+  free(balls);
+  free(grid);
 
   return EXIT_SUCCESS;
 }
