f8b283d2ce06a17de5c54b94048cdff54d7a0fa8
julian <julian.limburg@gmx.de>
2024-11-10 23:28:33 +0100

    better build logic

--- noob.c
+++ noob.c
@@ -3,8 +3,6 @@
 int main(int argc, const char *argv[]) {
   RebuildYourself(argc, argv);
 
-  int run = HasFlag(argc, argv, "run");
-
   if (HasFlag(argc, argv, "web")) {
     BuildAndRunCommand("rm -rf builds/web/* && emcc -o builds/web/yingyang.html src/ying-yang.c -Iraylib/src -Lraylib/src -lraylib "
                        "-s USE_GLFW=3 -s ASYNCIFY -s TOTAL_MEMORY=16777216 -DPLATFORM_WEB -DSUPPORT_TRACELOG=0 --shell-file src/shell.html");
@@ -16,10 +14,10 @@ int main(int argc, const char *argv[]) {
                        "-lGL -lm -lpthread "
                        "-ldl -lrt -lX11 "
                        "-o builds/ying-yang");
-  }
 
-  if (run)
-    BuildAndRunCommand("./builds/ying-yang");
+    if (HasFlag(argc, argv, "run"))
+      BuildAndRunCommand("./builds/ying-yang");
+  }
 
   return 0;
 }
